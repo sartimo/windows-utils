1313c5bc5df0398519ef159687ed091a36dbef07
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-12-10 11:07:46 +0100

    Update windows-utils-harpy.ps1

--- windows-utils-harpy/windows-utils-harpy.ps1
+++ windows-utils-harpy/windows-utils-harpy.ps1
@@ -1,14 +1,59 @@
-Function Harpy { 
+<#
+.SYNOPSIS
+An example of how to next commands.
 
-<# 
-    .Synopsis 
-        primitive but working static-site generator for powershell
-    .Description 
-         primitive but working static-site generator for powershell
-    .Example 
-        Harpy         
-        
-    URL: https://github.com/sartimo/windows-utils/blob/main/windows-utils-harpy/windows-utils-harpy.ps1
-    CMD: powershell.exe -ExecutionPolicy Bypass -File https://github.com/sartimo/windows-utils/blob/main/windows-utils-harpy/windows-utils-harpy.ps1
+.DESCRIPTION
+USAGE
+    .\4-nest.ps1 <command>
+
+COMMANDS
+    speak       speak some text
+    logs        view logs
 #>
+
+#################################### app parameters ############################################################
+param(
+    [Parameter(Position=0)]
+    [ValidateSet("init", "build", "serve")]
+    [string]$Command,
+
+    [Parameter(Position=1, ValueFromRemainingArguments=$true)]
+    $Rest
+)
+
+if (!$Command) {
+    Get-Help $PSCommandPath
+    exit
+}
+
+################################### function for initializing data structure ##################################
+function Command-Init {
+    param (
+        [Parameter(Position=0, Mandatory=$True)]
+        [string]$Directory
+    )
+
+
+    Write-Host "initializing Harpy structure at: $Directory"
+
+    if (!$Directory) {
+        Write-Host "initializing Harpy structure in current path"
+    }
+}
+
+################################## function for building static site ##########################################
+function Command-Build {
+    Write-Host "Building static site at .\build\*"
+}
+
+################################## function for serving static site locally ##################################
+function Command-Serve {
+    Write-Host "Serving site at http://localhost:8000"
+}
+
+################################## argument assignment #######################################################
+switch ($Command) {
+    "init" { Command-Init $Rest }
+    "build" { Command-Build $Rest }
+    "serve" { Command-Serve $Rest}
 }
