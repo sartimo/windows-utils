ab09c21bfc8e63d9911d533d6e71d3782649130c
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-12-10 10:43:44 +0100

    Create windows-utils-harpy.ps1

---
+++ windows-utils-harpy/windows-utils-harpy.ps1
@@ -0,0 +1,14 @@
+Function Harpy { 
+
+<# 
+    .Synopsis 
+        primitive but working static-site generator for powershell
+    .Description 
+         primitive but working static-site generator for powershell
+    .Example 
+        Harpy         
+        
+    URL: https://github.com/sartimo/windows-utils
+    CMD: powershell.exe -ExecutionPolicy Bypass -File 
+#>
+}
